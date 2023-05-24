7b3c982d72beb1dbb8e785e2d850d513995ec609
shima1028 <shima1028star@gmail.com>
2023-05-24 09:40:33 +0900

    adding reset and playing ippon sound features

--- public/js/result.js
+++ public/js/result.js
@@ -18,7 +18,7 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
 const host = location.origin.replace(/^http/, 'ws');
 const websocket = new WebSocket(host);
 const circles = document.querySelectorAll('.circle');
-let point_counter = 1;
+let point_counter = 0;
 
 console.log("hey!!");
 
@@ -28,17 +28,49 @@ websocket.onmessage = (event) => {
 
     const messageType = event.data.split(':')[0];
 
-    if (messageType === 'Received' && point_counter >= 1 && point_counter <= 10) {
+    if (messageType === 'Received' && point_counter >= 0 && point_counter < 10) {
         circles[point_counter].style.borderTop = 'solid rgb(250, 221, 1) 25px';
         circles[point_counter].style.borderRight = 'solid rgb(192, 169, 1) 25px';
         circles[point_counter].style.borderLeft = 'solid rgb(239, 209, 1) 25px';
         circles[point_counter].style.borderBottom = 'solid rgb(192, 169, 1) 25px';
         
-        const music = new Audio('sounds/'+ point_counter +'.mp3');
-        music.play();
+        let sound = new Audio('sounds/'+ (point_counter + 1) +'.mp3');
+        sound.play();
+
+        //ippon音声再生処理
+        console.log(point_counter);
+
+        if(point_counter === 8) {
+            function sleep(ms) {
+                return new Promise(resolve => setTimeout(resolve, ms));
+              }
+              
+              sleep(2000);
+              const ipponSound = new Audio('sounds/ippon.mp3');
+              ipponSound.play();
+
+        }
+        
+
         point_counter ++;
     }
         
 };
 
 
+
+//確定/リセットボタンに関する処理
+document.getElementById('confirm-button').addEventListener('click', function() {
+    if (this.innerHTML === '確定') {
+        this.innerHTML = 'リセット';
+    } else {
+        point_counter = 0;
+        let circleElements = document.getElementsByClassName('circle');
+        for (let i = 0; i < circleElements.length; i++) {
+            let circleElement = circleElements[i];
+            circleElement.style.display = "none";
+        }
+
+        this.innerHTML = '確定';
+    }
+});
